6d923ad9f734754a2dd4f7afadad51b37b919846
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-14 22:32:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// SNACK TRE
+// let stringa = prompt("scrivi una parola");
+
+// function giraStringa(string) {
+//     let arrayStr = [string.split("").reverse().join("")];
+//     let stringaGirata = arrayStr[0];
+//     return stringaGirata;
+// }
+// console.log(giraStringa(stringa));
+
+let primo = ["a", "b", "c", "d", "e", "f", "g"];
+let secondo = [1, 2, 3, 4, 5, 6, 7]
+let risultato = [];
+
+function concatenatore(first, second) {
+    let p = 0;
+    let s = 0;
+    while (p < primo.length && s < secondo.length) {
+        if () {
+            
+        }
+    }
+    
+}
+
+console.log(concatenatore(primo, secondo));
